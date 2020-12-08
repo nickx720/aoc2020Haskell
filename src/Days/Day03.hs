f43041cb5088cd848096737824a83448ed776324
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2020-12-08 11:57:35 +0000

    Day 3

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -1,7 +1,7 @@
 module Days.Day03 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
+import Data.List as L
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Maybe
@@ -9,11 +9,10 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
+import Util.Parsers (coordinateParser)
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +20,38 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = coordinateParser (flip lookup [('#',Tree),('.',Open)]) 0
 
 ------------ TYPES ------------
-type Input = Void
+data Square = Tree | Open deriving (Eq,Show)
+type Input = Map (Int,Int) Square
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+slope m x y = 
+    let width = (+ 1) . fst . maximum . Map.keys $ m
+        xs = fmap (`mod` width) [0,x..]
+        coords = zip xs [0,y..]
+    in length
+            .filter ( == Tree)
+            .fmap fromJust
+            . L.takeWhile (not. isNothing)
+            . fmap (m Map.!?)
+            $ coords
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA m = slope m 3 1
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB m = 
+    product . fmap (uncurry (slope m)) $
+    [
+        (1,1),
+        (3,1),
+        (5,1),
+        (7,1),
+        (1,2)
+    ]
