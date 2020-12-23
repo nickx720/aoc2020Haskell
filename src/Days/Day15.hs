0b92eb115eaf4b0d18b5eb2f4737557f96b23289
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2020-12-23 14:15:11 +0000

    Day 15th

--- src/Days/Day15.hs
+++ src/Days/Day15.hs
@@ -1,19 +1,13 @@
 module Days.Day15 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
+import qualified Data.Vector.Unboxed.Mutable as MVec
+import Control.Monad.State
+import Control.Monad.ST
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +15,33 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` char ','
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+playGame::[Int] -> Int-> Int
+playGame startingNumbers target = runST $ do
+    prevOccurenceVec <- MVec.replicate target (0::Int)
+    sequence_ $
+        fmap (uncurry $ MVec.write prevOccurenceVec) (zip startingNumbers [1..])
+    let rounds = [length startingNumbers + 1..target]
+    let performRound prevVal index = do
+            prevOccurence <- MVec.read prevOccurenceVec prevVal
+            ( return $ case prevOccurence of
+                0 -> 0
+                e -> (index - e - 1)
+                )
+                <* MVec.write prevOccurenceVec prevVal (index-1)
+    foldM performRound (last startingNumbers) rounds
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input = playGame input 2020
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input = playGame input 30_000_000
